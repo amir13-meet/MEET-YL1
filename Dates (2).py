3b91aa3efe6b689e6907035009ce622aa72c925d
amir13-meet <farfahman@gmail.com>
2013-11-25 00:37:43 +0200

    Fixed something in Dates

---
+++ Dates (2).py
@@ -0,0 +1,91 @@
+                                                                ### Dates
+
+#####################################################
+
+months_def = {}
+var = ""
+
+#####################################################
+
+def months():
+    global months_def
+    months_def["01"] = "January"
+    months_def["02"] = "February"
+    months_def["03"] = "March"
+    months_def["04"] = "April"
+    months_def["05"] = "May"
+    months_def["06"] = "June"
+    months_def["07"] = "July"
+    months_def["08"] = "August"
+    months_def["09"] = "September"
+    months_def["10"] = "October"
+    months_def["11"] = "November"
+    months_def["12"] = "December"
+
+#####################################################
+
+def SwapValue(month):
+    global months_def
+    global var
+    for k, v in months_def.iteritems():
+        if (month == k):
+            var = v
+    return var
+
+#####################################################
+
+def PrintDate_mm_dd(month, day, year):
+    print month + " " + day + " " + year
+
+#####################################################
+
+def PrintDate_dd_mm(day, month, year):
+    print day + " " + month + " " + year
+
+#####################################################    
+
+def mm_dd_yy():
+    string = AskDate()
+    months()
+    month = string[0:2] # string[0:2] // month in mm_dd_yy
+    day = string[3:5] # string[3:5] // day in mm_dd_yy
+    year = string[6:len(string)] # string[6:len(string)] //year
+    month = SwapValue(month)
+    PrintDate_mm_dd(month, day, year)
+
+#####################################################
+
+def dd_mm_yy():
+    string = AskDate()
+    months()
+    day = string[0:2] # string[0:2] // day in dd_mm_yy
+    month = string[3:5] # string[3:5] // month in dd_mm_yy
+    year = string[6:len(string)] # string[6:len(string)] //year
+    month = SwapValue(month)
+    PrintDate_dd_mm(day, month, year)
+
+#####################################################
+
+def AskDate():
+    x = raw_input("Enter the date: ")
+    return x
+
+#####################################################
+
+def AskDateKind():
+    answer = raw_input("mm_dd_yy  or  dd_mm_yy?: ")
+    while (answer != "mm_dd_yy") and (answer != "dd_mm_yy"):
+        print "Enter your answer exactly as printed for you"
+         answer = raw_input("mm_dd_yy  or  dd_mm_yy?: ")
+         
+    if (answer == "mm_dd_yy"):
+        mm_dd_yy()
+    else:
+        dd_mm_yy()
+
+#####################################################
+#####################################################
+
+if __name__ == "__main__":            
+    AskDateKind()
+    
